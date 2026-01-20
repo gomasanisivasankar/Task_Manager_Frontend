fd402fec0397d0991a731b295956ca49a3484c65
siva <ssankar6281@gmail.com>
2026-01-20 22:50:58 +0530

    added the url

--- src/Utils/Url.js
+++ src/Utils/Url.js
@@ -1,4 +1,6 @@
 export const Url = () => {
     // return process.env.REACT_APP_API_URL;
-    return "http://localhost:5000";
+    // return "http://localhost:5000";
+    // return "https://task-manager-backend-mauve.vercel.app"
+    return "https://task-manager-backend-u4d9.onrender.com"
   };
